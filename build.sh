8bb1a08dadca210609d689fed74fa1d999bcd504
sacha <sacha.laurent71@gmail.com>
2026-05-24 23:33:45 +0200

    💫 - Build de tous les fichiers en ASM du dossier aarch64

--- build.sh
+++ build.sh
@@ -13,13 +13,17 @@ else
     echo "Compilation pour vrai Raspberry Pi..."
 fi
 
-# Compilation AArch64
-aarch64-linux-gnu-gcc -DAARCH64 -c $ARCH_DIR/boot.S -o $BUILD_DIR/boot.o
+# Compilation AArch64 (tous les fichiers .S)
+for f in $ARCH_DIR/*.S; do
+    filename=$(basename "$f" .S)
+    aarch64-linux-gnu-gcc -DAARCH64 -c "$f" -o "$BUILD_DIR/$filename.o"
+done
+
 aarch64-linux-gnu-gcc -DAARCH64 $EXTRA_CFLAGS -std=gnu99 -ffreestanding -O2 -Wall -Wextra -c drivers/uart.c -o $BUILD_DIR/uart.o
 aarch64-linux-gnu-gcc -DAARCH64 $EXTRA_CFLAGS -std=gnu99 -ffreestanding -O2 -Wall -Wextra -c kernel/main.c -o $BUILD_DIR/kernel.o
 
-# Linking
-aarch64-linux-gnu-ld -T $ARCH_DIR/linker.ld --no-warn-rwx-segments -o $BUILD_DIR/kernel8.elf $BUILD_DIR/boot.o $BUILD_DIR/uart.o $BUILD_DIR/kernel.o
+# Linking (inclut tous les .o générés)
+aarch64-linux-gnu-ld -T $ARCH_DIR/linker.ld --no-warn-rwx-segments -o $BUILD_DIR/kernel8.elf $BUILD_DIR/*.o
 aarch64-linux-gnu-objcopy -O binary $BUILD_DIR/kernel8.elf $BUILD_DIR/kernel8.img
 
 echo "Build completed. Output: $BUILD_DIR/kernel8.img"
