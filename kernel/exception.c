c9bc5f35ae95c1c6dbedcf0e6940f91a5f905e4b
sacha <sacha.laurent71@gmail.com>
2026-06-25 00:57:13 +0200

    💫 - Création de la méthode default_handler et appel de l'initalisation du gic dans le main

--- kernel/exception.c
+++ kernel/exception.c
@@ -1,4 +1,5 @@
 #include <drivers/uart.h>
+#include <gic.h>
 
 void show_invalid_entry_message(int type, uint32_t esr, uint64_t elr) {
     uart_puts("Invalid exception entry: ");
@@ -13,4 +14,22 @@ void show_invalid_entry_message(int type, uint32_t esr, uint64_t elr) {
 
 void irq_handler(void) {
     uart_puts("Interruption reçue !\r\n");
+
+    volatile struct gic_distributor *gicd = (char *) GIC + 0x1000;
+    volatile struct gic_controller *gicc = (char *) GIC + 0x2000;
+
+    const uint32_t iar = gicc->IAR;
+
+    irq_callback_type cb = get_interrupt_handler(iar);
+    cb(iar);
+
+    if (iar == 1023) return;
+
+    gicc->EOIR = iar;
+};
+
+void default_handler(const uint32_t iar) {
+    uart_puts("Interruption reçue !\r\n");
+    uart_puts(" IAR: ");
+    uart_put_hex(iar);
 }
--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <drivers/uart.h>
+#include <gic.h>
 
 #ifdef AARCH64
 void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
@@ -13,6 +14,8 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 
     uart_init(4);
     uart_puts("Perrytel OS (AArch64) - Kernel Loaded\r\n");
+
+    gic_init();
 
     __asm__ volatile("brk #0");
 
